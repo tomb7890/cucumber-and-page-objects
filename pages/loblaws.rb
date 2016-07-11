850e149b420f26b3f447e0d57e2db8e2cf2ddb90
Tom Billingsley <tom.billingsley@gmail.com>
2016-07-11 06:10:01 -0400

    prefer cleaner hash syntax

--- pages/loblaws.rb
+++ pages/loblaws.rb
@@ -3,15 +3,15 @@ class LoblawsPage
   page_url 'http://shop.loblaws.ca'
   text_field(:search_bar, id: 'search_bar')
 
-  def search_for (search_term)
-    s = @browser.span :class => 'text-search'
+  def search_for(search_term)
+    s = @browser.span(class: 'text-search')
     s.click
-    @browser.text_field(:id => 'search-bar').when_present.set search_term
+    @browser.text_field(id: 'search-bar').when_present.set search_term
     @browser.form(:class, 'container form-nav-search').submit
   end
 
   def count_30cent_badges
     badge_text = 'SAVE $0.30'
-    @browser.elements(:class=> 'deal-type', :text => badge_text).size
+    @browser.elements(class: 'deal-type', text: badge_text).size
   end
 end
